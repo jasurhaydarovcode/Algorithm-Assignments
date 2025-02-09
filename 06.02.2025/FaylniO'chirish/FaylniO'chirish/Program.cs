7b0bf8ef5c05132bc1988e96e39f5dc97a7d322b
jasurhaydarovcode <jasurhaydarovcode@gmail.com>
2025-02-09 14:58:20 +0500

    added system.io --> delete method

---
+++ 06.02.2025/FaylniO'chirish/FaylniO'chirish/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.IO;
+using static System.Console;
+
+namespace FaylniO_chirish
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+            WriteLine("Ma'lumot Kiriting: ");
+            string inputData = Console.ReadLine();
+            string filePath = "input.txt";
+            try
+            {
+                File.WriteAllText(filePath, inputData);
+                WriteLine("Ma'lumot {0} ga muaffaqiyatli yozildi.", filePath);
+            }
+            catch(Exception e)
+            {
+                WriteLine("Xatolik yuz berdi.", e.Message);
+            }
+            */
+
+            
+            string filePath = "input.txt";
+            if(File.Exists(filePath))
+            {
+                File.Delete(filePath);
+                WriteLine("{0} Fayl muaffaqiyatli o'chirildi.", filePath);
+            }
+            else
+            {
+                WriteLine("{0} Fayl topilmadi.", filePath);
+            }
+            
+
+            // Console freeze
+            ReadLine();
+        }
+    }
+}
