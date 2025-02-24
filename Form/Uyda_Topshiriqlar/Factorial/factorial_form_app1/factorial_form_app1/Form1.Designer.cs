b9bca2d0865f53146f6eaafc1e9fe6a24ae872fc
jasurhaydarovcode <jasurhaydarovcode@gmail.com>
2025-02-24 15:17:15 +0500

    Factorial form app added code project homework

---
+++ Form/Uyda_Topshiriqlar/Factorial/factorial_form_app1/factorial_form_app1/Form1.Designer.cs
@@ -0,0 +1,140 @@
+﻿
+namespace factorial_form_app1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.label1.Font = new System.Drawing.Font("Chinese Rocks Rg", 22.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.Red;
+            this.label1.Location = new System.Drawing.Point(297, 24);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(356, 46);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Factorial Program.exe";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Chinese Rocks Rg", 17F, System.Drawing.FontStyle.Bold);
+            this.label2.ForeColor = System.Drawing.Color.Red;
+            this.label2.Location = new System.Drawing.Point(178, 129);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(269, 36);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Enter the Factorial: ";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Jokerman", 15F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.Color.White;
+            this.label3.Location = new System.Drawing.Point(178, 235);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(87, 37);
+            this.label3.TabIndex = 1;
+            this.label3.Text = "n! = x";
+            this.label3.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Chinese Rocks Rg", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.ForeColor = System.Drawing.Color.Red;
+            this.button1.Location = new System.Drawing.Point(183, 286);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(149, 47);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Calculate";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.BackColor = System.Drawing.Color.Red;
+            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))));
+            this.textBox1.ForeColor = System.Drawing.Color.Black;
+            this.textBox1.Location = new System.Drawing.Point(572, 130);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(176, 34);
+            this.textBox1.TabIndex = 3;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Chinese Rocks Rg", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.ForeColor = System.Drawing.Color.Red;
+            this.label4.Location = new System.Drawing.Point(178, 187);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(85, 28);
+            this.label4.TabIndex = 1;
+            this.label4.Text = "Answer: ";
+            this.label4.Click += new System.EventHandler(this.label4_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Black;
+            this.ClientSize = new System.Drawing.Size(984, 450);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Factorial";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label4;
+    }
+}
+
---
+++ Form/Uyda_Topshiriqlar/Factorial/factorial_form_app1/factorial_form_app1/Form1.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace factorial_form_app1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        // ======= First Way =======
+        //private void button1_Click(object sender, EventArgs e)
+        //{
+        //    int number = int.Parse(textBox1.Text);
+        //    long f = 1;
+        //    for (int i = 1; i <= number; i++)
+        //    {
+        //        f = f * i;
+        //    }
+        //    label3.Text = $"{number}! = {f}";
+        //}
+
+        // ======= Second Way =======
+        //private void button1_Click(object sender, EventArgs e)
+        //{
+        //    int number = int.Parse(textBox1.Text);
+        //    long factorial = CalculateFactorial(number);
+        //    label3.Text = factorial.ToString();
+        //}
+
+        // ======= CalculateFactorial Method =======
+        //private long CalculateFactorial(int n)
+        //{
+        //    if (n == 0 || n == 1)
+        //        return 1;
+
+        //    long result = 1;
+        //    for (int i = 2; i <= n; i++)
+        //    {
+        //        result *= i;
+        //    }
+        //    return result;
+        //}
+
+        // Third Way
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                int number = int.Parse(textBox1.Text);
+                long factorial = Factorial(number);
+                //label3.Text = factorial.ToString();
+                //MessageBox.Show($"{number}! = {factorial}");
+                label3.Text = $"{number}! = {factorial}";
+            }
+            catch(Exception ex)
+            {
+                label3.Text = "Xatolik: " + ex.Message;
+            }
+            
+        }
+
+        static long Factorial(int number)
+        {
+            if (number == 1)
+            {
+                return 1;
+            }
+            else
+            {
+                return number * Factorial(number - 1);
+            }
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
